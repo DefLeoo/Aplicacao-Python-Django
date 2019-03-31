619d4cea731a38e38d745b97145767c941eda325
DefLeoo <lnddru@gmail.com>
2019-03-31 09:25:15 -0300

    Primeiro commit aplicação Django

---
+++ appDjango/boletim/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import Registrado
+
+# Register your models here.
+class AdminRegistrado(admin.ModelAdmin):
+	list_display  = ["email", "nome", "timestamp"]
+	list_filter = ["timestamp"]
+	list_editable = ["nome"]
+	search_fields = ["email", "nome"]
+	class Meta:
+		model = Registrado
+
+
+
+
+admin.site.register(Registrado, AdminRegistrado)
